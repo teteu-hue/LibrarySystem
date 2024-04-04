32d1463c9e8eb0b2eb87cffcc2870d6abe6ba301
RodrigoSantanadeCastro <rodrigo.santana58@etec.sp.gov.br>
2024-04-04 17:17:06 -0300

    ideia inicial de design

--- view/formCreateBook.php
+++ view/formCreateBook.php
@@ -14,13 +14,13 @@
 
 <form action="acao.php" method="post">
     <pre class="form-group">
-        <label class="form-label" for="">Nome do Livro</label>
-        <input class="form-control" type="text" name="nameBook" id="">
+        <label class="form-label fs-2" for="">Nome do Livro</label>
+        <input class="form-control form-control-lg fs-4 " type="text" name="nameBook" id="">
 
-        <select class="form-select"  name="genderBook" size="<?php count($genderBooks)?>" id="">          
+        <select class="form-control form-control-lg fs-2"  name="genderBook" size="<?php count($genderBooks)?>" id="">          
             <?php for($i = 0; $i < count($genderBooks); $i++){ ?>
                 
-                <option class="option" value="<?php echo $genderBooks[$i]->getIdGenderBook()?>">
+                <option class="option fs-2" value="<?php echo $genderBooks[$i]->getIdGenderBook()?>">
                     <?php echo $genderBooks[$i]->getName()?>
                 </option>
 
@@ -28,14 +28,14 @@
 
         </select>
 
-        <label class="form-label" for="">Preço do Livro</label>
-        <input class="form-control" type="number" step="0.01" name="price">
+        <label class="form-label fs-2" for="">Preço do Livro</label>
+        <input class="form-control form-control-lg fs-4" type="number" step="0.01" name="price">
         
 
-        <label class="form-label" for="">Número de Páginas</label>
-        <input class="form-control" type="number" name="numPage">
+        <label class="form-label fs-2" for="">Número de Páginas</label>
+        <input class="form-control form-control-lg fs-4" type="number" name="numPage">
 
-        <button class="btn btn-primary" type="submit">Enviar</button> <button class="btn btn-primary" type="reset">Limpar</button>
+        <button class="btn btn-primary fs-5" type="submit">Enviar</button> <button class="btn btn-primary fs-5" type="reset">Limpar</button>
 
     </pre>
 
