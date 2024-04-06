91e41be5d2f195198bcba61404f58e8550de837d
RodrigoSantanadeCastro <rodrigo.santana58@etec.sp.gov.br>
2024-04-06 18:54:22 -0300

    login page

---
+++ view/login.php
@@ -0,0 +1,29 @@
+<?php include "header.php" ?>
+
+
+
+<H1 class="m-3 fs-1">Login</H1>
+<form name="login" action="post">
+        <div>
+            <label class=" m-3 form-label fs-5" for="">E-Mail:</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="email" placeholder="Digite seu E-mail" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">Senha:</label>
+            <input class=" m-3 form-control form-control-lg fs-5"  name="senha" type="password" placeholder="Digite sua senha"  >
+        </div>
+        <div>
+
+            <button class=" m-3 btn btn-primary fs-5" type="submit">Enviar</button> <button class="  btn btn-primary fs-5" type="reset">Limpar</button>
+        </div>
+
+    </form>
+
+
+
+
+
+
+
+
+<?php require("footer.php")?>
