5f8743fbe0db37ec3ca0f6da51e3350e067a6e62
RodrigoSantanadeCastro <rodrigo.santana58@etec.sp.gov.br>
2024-04-10 07:48:24 -0300

    pessoa run querry

--- Database/database_connect.php
+++ Database/database_connect.php
@@ -73,6 +73,31 @@ if($mysqli->query($livroQuery) === TRUE){
     echo "Erro ao criar tabela: " . $mysqli->error;
 }
 
+if($mysqli->query($contatoQuery) === TRUE){
+    echo "Tabela criada com sucesso. <br>";
+} else {
+    echo "Erro ao criar tabela: " . $mysqli->error;
+}
+
+if($mysqli->query($pessoaQuery) === TRUE){
+    echo "Tabela criada com sucesso. <br>";
+} else {
+    echo "Erro ao criar tabela: " . $mysqli->error;
+}
+
+if($mysqli->query($devolucaoQuery) === TRUE){
+    echo "Tabela criada com sucesso. <br>";
+} else {
+    echo "Erro ao criar tabela: " . $mysqli->error;
+}
+
+if($mysqli->query($emprestimoQuery) === TRUE){
+    echo "Tabela criada com sucesso. <br>";
+} else {
+    echo "Erro ao criar tabela: " . $mysqli->error;
+}
+
+
 // Inserindo dados nas tabelas
 $sql_insert_genero = "INSERT INTO Genero (nome_genero) VALUES 
     ('Ficção Científica'),
@@ -88,6 +113,7 @@ $sql_insert_livro = "INSERT INTO Livro (nome_livro, id_genero, preco, numeroDePa
     ('O Código Da Vinci', 4, 39.50, 480, 'Um suspense cheio de mistérios e reviravoltas'),
     ('Uma Breve História do Tempo', 5, 34.50, 256, 'Explorações no campo da física teórica por Stephen Hawking'),
     ('1987', 3, 34.50, 256, 'Mil novecentos e oitenta e quatro (também publicado como 1984) é um romance distópico do escritor inglês George Orwell. Foi publicado em 8 de junho de 1949 pela Secker & Warburg como o nono e último livro de Orwell concluído em vida. Tematicamente, centra-se nas consequências do totalitarismo, da vigilância em massa e da lavagem cerebral na sociedade')";
+
 
 //if($mysqli->query($sql_insert_genero) === TRUE){
 //    echo "Dados inseridos com sucesso!<br>";
