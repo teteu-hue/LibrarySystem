5ce22a6baac47411b3f523b26af9151c280f8d4c
RodrigoSantanadeCastro <rodrigo.santana58@etec.sp.gov.br>
2024-04-14 10:41:50 -0300

    Cadastro update

--- view/cadastro.php
+++ view/cadastro.php
@@ -21,12 +21,84 @@
 
     <form name="cadastro" onsubmit="senhaOK()" action="post">
         <div>
-            <label class=" m-3 form-label fs-5" for="">Nome:</label>
+            <label class=" m-3 form-label fs-5" for="">Nome Completo:</label>
             <input class=" m-3 form-control form-control-lg fs-5" type="text" placeholder="Digite seu nome" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">Qual é sua Idade</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="text" placeholder="Digite sua idade" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">Data de Nascimento</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="date" placeholder="Digite quando você nasceu" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">CPF</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="text" placeholder="Digite seu CPF" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">RG</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="text" placeholder="Digite seu RG" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">Telefone/Celular</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="tel" placeholder="Digite seu telefone" >
         </div>
         <div>
             <label class=" m-3 form-label fs-5" for="">E-mail:</label>
             <input class=" m-3 form-control form-control-lg fs-5" type="email" placeholder="Digite o seu E-mail" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">Rua</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="text" placeholder="Digite sua rua" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">Numero da Residencia</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="text" placeholder="Digite o numero da sua residencia" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">Complemento</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="text" placeholder="Digite o complemento" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">Cidade</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="text" placeholder="Digite sua cidade" >
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">Estado</label>
+            <select id="estado" name="estado">
+                <option value="AC">AC</option>
+                <option value="AL">AL</option>
+                <option value="AP">AP</option>
+                <option value="AM">AM</option>
+                <option value="BA">BA</option>
+                <option value="CE">CE</option>
+                <option value="DF">DF</option>
+                <option value="ES">ES</option>
+                <option value="GO">GO</option>
+                <option value="MA">MA</option>
+                <option value="MT">MT</option>
+                <option value="MS">MS</option>
+                <option value="MG">MG</option>
+                <option value="PA">PA</option>
+                <option value="PB">PB</option>
+                <option value="PR">PR</option>
+                <option value="PE">PE</option>
+                <option value="PI">PI</option>
+                <option value="RJ">RJ</option>
+                <option value="RN">RN</option>
+                <option value="RS">RS</option>
+                <option value="RO">RO</option>
+                <option value="RR">RR</option>
+                <option value="SC">SC</option>
+                <option value="SP">SP</option>
+                <option value="SE">SE</option>
+                <option value="TO">TO</option>
+            </select>
+        </div>
+        <div>
+            <label class=" m-3 form-label fs-5" for="">CEP</label>
+            <input class=" m-3 form-control form-control-lg fs-5" type="text" placeholder="Digite seu CEP" >
         </div>
         <div>
             <label class=" m-3 form-label fs-5" for="">Senha:</label>
