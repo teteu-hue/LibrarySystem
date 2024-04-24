a0483e478409fd2e360529a315062cc4874cab46
RodrigoSantanadeCastro <rodrigo.santana58@etec.sp.gov.br>
2024-04-24 20:37:19 -0300

    devolução/emprestimo form

---
+++ view/devolução.php
@@ -0,0 +1,27 @@
+<?php include "header.php" ?>
+
+<h1 class="m-3 fs-1"> Devolução de Livros</h1>
+
+    <form action="">
+        <label class=" m-3 form-label fs-5" for="">Cliente</label>
+        <input class=" m-3 form-control form-control-lg fs-5" type="text">
+
+        <label class=" m-3 form-label fs-5" for="">Livro</label>
+        <input class=" m-3 form-control form-control-lg fs-5" type="text">
+
+        <label class=" m-3 form-label fs-5" for="">Data de devolução</label>
+        <input class=" m-3 form-control form-control-lg fs-5" type="date">
+
+    
+    </form>
+
+    <div>
+
+        <button class=" m-3 btn btn-outline-success btn-sm fs-5" type="submit">Enviar</button> <button class="m-3 btn btn-outline-danger btn-sm fs-5" type="reset">Limpar</button>
+    </div>
+
+
+
+
+
+<?php require("footer.php")?>
---
+++ view/inicial.php
@@ -0,0 +1,31 @@
+<?php include "header.php" ?>
+
+<div class="container text-center">
+    <div class="col">
+     <h1>Bem vindo ao sistema de gerenciamento do Matheus e Rodrigo</h1>      
+    </div>
+</div>
+
+<div class="border rounded">
+    <div>
+        <p class="h6">Deseja fazer login ?</p>
+        <a href="/LibrarySystem/view/login.php">
+            <button type="button" class="btn btn-primary btn-sm">Login</button>
+        </a>
+    </div>
+    <div>
+        <p class="h6">Novo por aqui ?</p>
+        <a href="/LibrarySystem/view/cadastro.php">
+             <button type="button" class="btn btn-primary btn-sm" >cadastre-se</button>
+        </a>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+<?php require("footer.php")?>
