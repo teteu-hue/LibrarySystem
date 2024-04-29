4b37df109f3de8a65490b991b8897e3daec3b181
RodrigoSantanadeCastro <rodrigo.santana58@etec.sp.gov.br>
2024-04-29 15:28:54 -0300

    Login_Querry_Atempt

---
+++ view/testLogin.php
@@ -0,0 +1,23 @@
+<?php
+   // print_r($_REQUEST);
+
+    if(isset($_POST['submit']) && empty($_POST['email']) && empty($_POST['senha'])){
+
+        include_once('../Database/Database.php');
+        $conn= Databese::getConnection();
+        
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        $sql = "SELECTED * FROM Contato AND Usuario email=$email and senha=$senha";
+
+        $result = $conn->querry($sql);
+
+        print_r ($result);
+
+    }
+    else{
+        header('Location: login.php');
+
+    } 
+?>
