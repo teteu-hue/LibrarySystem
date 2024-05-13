e5be4d5e9b9e2d8fd2cbb807496b983bd9844c13
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-05-13 15:35:11 -0300

    create-form-to-delete-and-edit-book

--- view/menu-admin-book.php
+++ view/menu-admin-book.php
@@ -1,4 +1,15 @@
-<?php require_once("header.php") ?>
+<?php require_once("header.php");
+
+require_once("../controller/RootDir/RootDir.php");
+
+$rootDir = RootDir::getRealPath();
+
+require_once("$rootDir/model/Book/DbBook.php");
+
+$dBook = new DbBook();
+$books = $dBook->getAllBook();
+
+?>
 <link rel="stylesheet" href="/LibrarySystem/css/menu-admin.css">
 
 <div class="row">
@@ -14,7 +25,7 @@
                 <a href="formCreateBook.php" class="btn btn-success buttom-item">
                     <i class="fa-solid fa-plus"></i>
                     <span class="button-action">Criar</span>
-                </a >
+                </a>
             </div>
 
         </div>
@@ -30,49 +41,29 @@
 
                     <div class="dropdown-menu container-publisher">
 
-                        <div class="dropdown-item  list-group-item d-flex justify-content-between align-items-start">
-                            <div class="ms-2 me-auto">
-                                <div class="fw-bold">Subheading</div>
-                                Content for list item
-                            </div>
-                            <div class="d-flex justify-content-evenly" style="width: 10rem;">
-                                <a class="badge bg-danger rounded-pill">
-                                    <i class="fa-solid fa-xmark"></i>
-                                </a>
-                                <a class="badge bg-warning rounded-pill">
-                                    <i class="fa-solid fa-xmark"></i>
-                                </a>
-                            </div>
-                        </div>
+                        <?php foreach ($books as $row) { ?>
+                            <div class="dropdown-item  list-group-item d-flex justify-content-between align-items-end">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold"><?php echo $row['nome_livro'] ?></div>
+                                    <?php echo $row['descricao'] ?>
+                                </div>
+                                <div class="d-flex justify-content-evenly" style="width: 10rem;">
+                                    <form method="POST" action="/LibrarySystem/controller/Book/delete.php?id_livro=<?php echo $row["id_livro"] ?>">
 
-                        <div class="dropdown-item list-group-item d-flex justify-content-between align-items-start">
-                            <div class="ms-2 me-auto">
-                                <div class="fw-bold">Subheading</div>
-                            </div>
+                                        <button type="submit" class="badge bg-danger rounded-pill">
+                                            <i class="fa-solid fa-xmark"></i>
+                                        </button>
+                                    </form>
 
-                            <div class="d-flex justify-content-evenly" style="width: 10rem;">
-                                <a class="badge bg-danger rounded-pill">
-                                    <i class="fa-solid fa-xmark"></i>
-                                </a>
-                                <a class="badge bg-warning rounded-pill">
-                                    <i class="fa-solid fa-xmark"></i>
-                                </a>
-                            </div>
-                        </div>
+                                    <form method="POST" action="/LibrarySystem/controller/Book/get.php?id_livro=<?php echo $row["id_livro"] ?>">
+                                        <button type="submit" class="badge bg-warning rounded-pill">
+                                            <i class="fa-solid fa-pen"></i>
+                                        </button>
+                                    </form>
 
-                        <div class="dropdown-item list-group-item d-flex justify-content-between align-items-start">
-                            <div class="ms-2 me-auto">
-                                <div class="fw-bold">Subheading</div>
+                                </div>
                             </div>
-                            <div class="d-flex justify-content-evenly" style="width: 10rem;">
-                                <a class="badge bg-danger rounded-pill">
-                                    <i class="fa-solid fa-xmark"></i>
-                                </a>
-                                <a class="badge bg-warning rounded-pill">
-                                    <i class="fa-solid fa-xmark"></i>
-                                </a>
-                            </div>
-                        </div>
+                        <?php } ?>
 
                     </div>
 
