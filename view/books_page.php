c335bd043eb9b5a0782aa095de2598dbd5da8be5
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-05-16 20:27:01 -0300

    showing-the-books-in-the-page'books_page.php'

--- view/books_page.php
+++ view/books_page.php
@@ -1,4 +1,14 @@
-<?php require("header.php") ?>
+<?php
+require_once("header.php");
+require_once("../controller/RootDir/RootDir.php");
+$rootDir = RootDir::getRealPath();
+
+require_once("$rootDir/model/Book/DbBook.php");
+$db = new DbBook();
+
+$books = $db->getAllBook();
+
+?>
 
 <style>
     .row {
@@ -23,33 +33,27 @@
     </div>
 </div>
 
-<div class="container text-center">
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">An item</li>
-                        <li class="list-group-item">A second item</li>
-                        <li class="list-group-item">A third item</li>
-                    </ul>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+<div class="container text-center d-flex justify-content align-items" style="width: 90%;">
+    <div class="row d-flex flex-flow" style="width:100%">
 
-            </div>
-        </div>
+        <?php foreach ($books as $row) { ?>
 
-        <div class="col">
             <div class="col">
-                <div class="card">
+                <div class="card" style="width: 20rem; margin-top:2rem;">
                     <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title"><?php echo $row["nome_livro"] ?></h5>
+                        
+                        <?php if ($row["descricao"] == NULL) { ?>
+
+                            <p class="card-text text-danger">Nenhuma Descrição cadastrada</p>
+
+                        <?php } else { ?>
+
+                            <p class="card-text text-danger"><?php $row["descricao"] ?></p>
+
+                        <?php } ?>
+
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">An item</li>
                             <li class="list-group-item">A second item</li>
@@ -57,90 +61,15 @@
                         </ul>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
+
                 </div>
             </div>
 
-        </div>
+        <?php }; ?>
 
-        <div class="col">
-            <div class="col">
-                <div class="card">
-                    <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
 
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">An item</li>
-                            <li class="list-group-item">A second item</li>
-                            <li class="list-group-item">A third item</li>
-                        </ul>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-        </div>
     </div>
 
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">An item</li>
-                        <li class="list-group-item">A second item</li>
-                        <li class="list-group-item">A third item</li>
-                    </ul>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="col">
-                <div class="card">
-                    <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">An item</li>
-                            <li class="list-group-item">A second item</li>
-                            <li class="list-group-item">A third item</li>
-                        </ul>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="col">
-            <div class="col">
-                <div class="card">
-                    <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">An item</li>
-                            <li class="list-group-item">A second item</li>
-                            <li class="list-group-item">A third item</li>
-                        </ul>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
 </div>
 
 <?php require("footer.php") ?>
