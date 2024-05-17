d0c33c90b1494a10b4cca3ded2210a1a0c43248b
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-05-17 15:58:37 -0300

    commenting-sql-script

--- Database/Tables_script.sql
+++ Database/Tables_script.sql
@@ -11,6 +11,7 @@ DROP TABLE Livro;
 DROP TABLE Genero;
 DROP TABLE Editora;
 
+-- tabela de livros ************************************
 CREATE TABLE Genero(
 	id_genero SERIAL PRIMARY KEY,
     nome_genero VARCHAR(50) NOT NULL
@@ -41,20 +42,24 @@ CREATE TABLE Assunto(
     nome_assunto VARCHAR (22) NOT NULL
 );
 
-
 CREATE TABLE Livro_assunto(
-	FOREIGN KEY (id_assunto)
-	REFERENCES Assunto(id_assunto),
-    FOREIGN KEY (id_livro) 
-    REFERENCES Livro(id_livro)
+    id_assunto INT NOT NULL,
+    id_Livro INT NOT NULL,
+	FOREIGN KEY (id_assunto) REFERENCES Assunto(id_assunto),
+    FOREIGN KEY (id_livro) REFERENCES Livro(id_livro)
 );
+-- ***********************************
 
+-- tabela de empréstimo
 CREATE TABLE Emprestimo(
 	id_emprestimo SERIAL PRIMARY KEY,
     data_emprestimo DATETIME NOT NULL,
     data_devolucao DATE NOT NULL,
+    id_pessoa INT NOT NULL,
     FOREIGN KEY (id_livro) 
-    REFERENCES Livro(id_livro)
+    REFERENCES Livro(id_livro),
+    FOREIGN KEY (id_pessoa)
+    REFERENCES Pessoa(id_pessoa)
 );
 
 CREATE TABLE Devolucao(
@@ -63,23 +68,40 @@ CREATE TABLE Devolucao(
     FOREIGN KEY (id_emprestimo) 
     REFERENCES Emprestimo(id_emprestimo)
 );
+-- ****************************
 
+-- tabela pessoa que será associado tanto em cliente quanto em funcionário
 CREATE TABLE Pessoa(
 	id_pessoa SERIAL PRIMARY KEY,
     nome_pessoa VARCHAR (45) NOT NULL,
     idade INT (3) NOT NULL,
     rg VARCHAR (12) NOT NULL,
     cpf VARCHAR (14) NOT NULL,
-    data_nascimento DATE NOT NULL
+    data_nascimento DATE NOT NULL,
+    id_contato INT NULL,
+    FOREIGN KEY (id_contato) REFERENCES Contato(id_contato)
 );
 
+-- tabela cliente
 CREATE TABLE Cliente(
 	id_cliente SERIAL PRIMARY KEY,
     matricula VARCHAR (45) NOT NULL,
+    id_pessoa INT NOT NULL,
+    FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id_pessoa)
+);
+
+-- tabela funcionário
+CREATE TABLE Funcionario(
+	id_funcionario SERIAL PRIMARY KEY,
+    cod_interno VARCHAR (30) NOT NULL,
+    id_pessoa INT NOT NULL,
     FOREIGN KEY (id_pessoa) 
     REFERENCES Pessoa(id_pessoa)
 );
 
+-- ***************************
+
+-- tabela de endereco
 CREATE TABLE Endereco(
 	id_endereco SERIAL PRIMARY KEY,
     logradouro VARCHAR (45) NOT NULL,
@@ -87,44 +109,31 @@ CREATE TABLE Endereco(
     complemento VARCHAR (45) NOT NULL,
     cep VARCHAR (8) NOT NULL
 );
-
+-- tabela que é usado para relacionar Pessoa com Endereço
+-- Um endereço pode ter muitas pessoas
+-- Uma pessoa pode ter muitos endereços
 CREATE TABLE Pessoa_has_Endereco(
 	FOREIGN KEY (id_pessoa) 
     REFERENCES Pessoa(id_pessoa),
     FOREIGN KEY (id_endereco) 
     REFERENCES Endereco(id_endereco)
 );
-
+-- tabela de contato
+-- toda pessoa só poderá ter um meio de contato no website
 CREATE TABLE Contato(
 	id_contato SERIAL PRIMARY KEY,
     email VARCHAR (40) NOT NULL,
     telefone VARCHAR (11) NOT NULL,
-    FOREIGN KEY (id_pessoa) 
-    REFERENCES Pessoa(id_pessoa)
+    FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id_pessoa)
 );
 
-CREATE TABLE Funcionario(
-	id_funcionario SERIAL PRIMARY KEY,
-    cod_interno VARCHAR (30) NOT NULL,
-    FOREIGN KEY (id_pessoa) 
-    REFERENCES Pessoa(id_pessoa)
-);
-
+-- tabela de usuario, será utilizada para logar no website
 CREATE TABLE Usuario(
 	id_usuario SERIAL NOT NULL,
-	senha VARCHAR (45) NOT NULL
-);
-
-Alter table Emprestimo
-Add constraint fk_id_pessoa foreign key (id_pessoa)
- references Pessoa (id_pessoa);
- 
- Alter table Usuario
-Add constraint fk_id_funcionario foreign key (id_funcionario)
- references Funcionario (id_funcionario);
-    
-
-
+    id_pessoa INT,
+	senha VARCHAR (45) NOT NULL,
+    FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id_pessoa)
+); 
 
 INSERT INTO Genero(nome_genero) 
 VALUES 
