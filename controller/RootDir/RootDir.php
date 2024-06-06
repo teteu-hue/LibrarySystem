0fa5916a846f2a4b4f813963550848d4ebc74383
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-06-06 19:43:27 -0300

    .

--- controller/RootDir/RootDir.php
+++ controller/RootDir/RootDir.php
@@ -5,7 +5,7 @@
 
 
         public static function getRealPath(){
-            return realpath($_SERVER["DOCUMENT_ROOT"]) . "/LibrarySystem";
+            return realpath($_SERVER["DOCUMENT_ROOT"]);
         }
     }
 
