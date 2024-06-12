efd8f02c0ffccb7ab12f8ad117f66b7f282717df
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-06-12 19:16:07 -0300

    .

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require_once("controller/RootDir/RootDir.php");
     $rootDir = RootDir::getRealPath();
     
-    require_once("$rootDir/model/Book/DbBook.php");
+    require_once("/LibrarySystem/model/Book/DbBook.php");
     require_once("$rootDir/model/Book/DbGenderBook.php");
     require_once("$rootDir/model/Book/DbPublisherBook.php");
     require_once("$rootDir/Database/Database.php");
